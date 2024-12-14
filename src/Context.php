c31bd82a735ce003e355c1d75dc993c01be5a4d5
Tobias Bachert <git@b-privat.de>
2024-12-15 00:56:00 +0100

    Add `LoggerProviderInterface` to context

--- src/Context.php
+++ src/Context.php
@@ -1,6 +1,8 @@
 <?php declare(strict_types=1);
 namespace Nevay\OTelSDK\Configuration;
 
+use OpenTelemetry\API\Logs\LoggerProviderInterface;
+use OpenTelemetry\API\Logs\NoopLoggerProvider;
 use OpenTelemetry\API\Metrics\MeterProviderInterface;
 use OpenTelemetry\API\Metrics\Noop\NoopMeterProvider;
 use OpenTelemetry\API\Trace\NoopTracerProvider;
@@ -16,11 +18,13 @@ final class Context {
     /**
      * @param TracerProviderInterface $tracerProvider tracer provider to use for self diagnostics
      * @param MeterProviderInterface $meterProvider meter provider to use for self diagnostics
+     * @param LoggerProviderInterface $loggerProvider logger provider to use for self diagnostics
      * @param LoggerInterface $logger logger to use for self diagnostics
      */
     public function __construct(
         public readonly TracerProviderInterface $tracerProvider = new NoopTracerProvider(),
         public readonly MeterProviderInterface $meterProvider = new NoopMeterProvider(),
+        public readonly LoggerProviderInterface $loggerProvider = new NoopLoggerProvider(),
         public readonly LoggerInterface $logger = new NullLogger(),
     ) {}
 }
